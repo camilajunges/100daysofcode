66d1bc8cefa1b5ec66a80d6e6ea0750d9455e918
camilajunges <camilasjunges@gmail.com>
2025-08-14 23:02:00 -0300

    ✨ Add functionality to check if a number is odd or even in ex003.py

--- Day-003/ex003.py
+++ Day-003/ex003.py
@@ -6,3 +6,14 @@ if height > 120:
     print("You have height enough!")
 else:
     print("Sorry you don't have height enough...")
+
+
+
+# modulo operator (how many is remaining after the division)
+# Write some code using modulp op and conditional checks to check if the number in the input area is odd or even. If it's odd print the word 'odd' otherwise print 'even'
+
+number = int(input("Write a number: "))
+if number % 2 == 0:
+  print('Odd')
+else:
+  print('Even')
