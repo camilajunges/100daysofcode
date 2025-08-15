ff03ea6351bb6eaba1b7431dfff3e1c0aa781bcd
camilajunges <camilasjunges@gmail.com>
2025-08-14 22:22:45 -0300

    Add conditional statements and height validation in ex003.py

---
+++ Day-003/ex003.py
@@ -0,0 +1,8 @@
+# conditional statements, logical operators, code blocks and scope
+print("See if you got the enough height here.")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You have height enough!")
+else:
+    print("Sorry you don't have height enough...")
